7559cea38cc68773be5ae70f55932cc5f30f6492
Mounir <85800425+Mounir-mk@users.noreply.github.com>
2023-01-11 17:00:45 +0100

    adding register in backend

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,9 @@ router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
+const userControllers = require("./controllers/userControllers");
+const hashPassword = require("./services/auth");
+
+router.post("/users", hashPassword, userControllers.add);
+
 module.exports = router;
